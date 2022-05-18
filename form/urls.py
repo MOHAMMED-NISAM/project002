00ea3ac122f8f12cc997f3c78037f4b54adc8f5b
MOHAMMED-NISAM <nisamtc007@gmail.com>
2022-05-18 17:27:47 +0530

    first commit

---
+++ form/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url,include
+from . import views 
+
+urlpatterns = [
+    url('sample',views.sampleFun,name='sample'),
+    url('index',views.indexFun,name='index'),
+    url('home' ,views.homeFun,name = 'home')
+]
---
+++ form/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Welcome')
+
+
+def sampleFun(request):
+    return render(request, 'form.html')
+
+
+def indexFun(request):
+    return render(request, 'index.html')
+
+def homeFun(request):
+    return render(request, 'home.html')
